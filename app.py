27316a956434703cebd81ec64386d2b6bd4e5e65
SWAPPEDBOI <hannawoo2010@gmail.com>
2026-07-11 16:23:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ def reset_all():
     st.session_state.user_name = ""
     st.session_state.weather = "맑음"
     st.session_state.top_type = "후드티"
-    st.session_state.bottom_type = "청바지
+    st.session_state.bottom_type = "청바지"
     st.session_state.top_color = "밝음"
     st.session_state.bottom_color = "슬림"
     st.session_state.shoes_type = "스니커즈"
