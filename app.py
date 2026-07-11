81a8453242eafe5a1f38bcf19cd25df134be0af6
SWAPPEDBOI <hannawoo2010@gmail.com>
2026-07-11 15:24:31 +0900

    Change text input to text area for feedback

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ grade = st.radio("학년", ["1", "2", "3"], horizontal=True)
 klass = st.number_input("반", value=1)
 difficulty = st.select_slider("난이도",options=["낮음", "보통", "높음"])
 score = st.slider("점수", 0, 100,50)
-sogam = st.text_input("소감")
+sogam = st.text_area("소감")
 
 if st.button("확인"):
     st.success(f"{name}/{grade}학년/{klass}반/{difficulty}")
